27b9ebe55169253fa3fa2aeaf593d1901d02e563
AtharvSingh <atharvsingh1396@gmail.com>
2025-08-08 21:19:39 +0530

    first commit

---
+++ CHIP-8_Emulator.cpp
@@ -0,0 +1,12 @@
+﻿// CHIP-8_Emulator.cpp : Defines the entry point for the application.
+//
+
+#include "CHIP-8_Emulator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
