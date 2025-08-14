e201678245581d894548a84db95e1980715e3755
AtharvSingh <atharvsingh1396@gmail.com>
2025-08-14 15:16:09 +0530

    6xkk

--- Chip8.cpp
+++ Chip8.cpp
@@ -119,6 +119,14 @@ void Chip8::OP_4xkk()
 	}
 }
 
+void Chip8::OP_6xkk()
+{
+	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+	uint8_t byte = opcode & 0x00FFu;
+
+	registers[Vx] = byte;
+}
+
 void Chip8::OP_5xy0()
 {
 	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
