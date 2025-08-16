655a5f9c38a847bd7b641c982eac21aa5ac05408
AtharvSingh <atharvsingh1396@gmail.com>
2025-08-16 14:11:16 +0530

    OR instruction

--- Chip8.cpp
+++ Chip8.cpp
@@ -154,6 +154,14 @@ void Chip8::OP_8xy0()
 	registers[Vx] = registers[Vy];
 }
 
+void Chip8::OP_8xy1()
+{
+	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+	uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+	registers[Vx] |= registers[Vy];
+}
+
 class Chip8
 {
 	Chip8()
