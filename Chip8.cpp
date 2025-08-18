41a80a6c5be7b009b78a969e9b92be7e932bf013
AtharvSingh <atharvsingh1396@gmail.com>
2025-08-18 22:35:25 +0530

    SUB instruction

--- Chip8.cpp
+++ Chip8.cpp
@@ -197,6 +197,23 @@ void Chip8::OP_8xy4()
 	registers[Vx] = sum & 0xFFu;
 }
 
+void Chip8::OP_8xy5()
+{
+	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+	uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+	if (registers[Vx] > registers[Vy])
+	{
+		registers[0xF] = 1;
+	}
+	else
+	{
+		registers[0xF] = 0;
+	}
+
+	registers[Vx] -= registers[Vy];
+}
+
 class Chip8
 {
 	Chip8()
